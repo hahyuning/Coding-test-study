8f3d192ced4ef8e969c4111caafc053fd7da4f44
hahyuning <hahyuning@gmail.com>
2021-12-11 18:38:18 +0900

    2021.12.11

--- daily_problem/211211/211211_1.py
+++ daily_problem/211211/211211_1.py
@@ -1,5 +1,41 @@
-def solution(n):
-    k = n ** 2
-    return 2 * k - 1 + 2 * (k - 1) - 1 + 2 * (k - 2) - 1
+import sys
+input = sys.stdin.readline
 
-print(solution(4))
+def find(x):
+    if x != parent[x]:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+edges = []
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+edges.sort()
+
+ans = 0
+s = 0
+for c, a, b in edges:
+    s += c
+    if find(a) != find(b):
+        ans += c
+        union(a, b)
+
+tmp = set()
+for i in range(1, n + 1):
+    tmp.add(find(i))
+if len(tmp) != 1:
+    print(-1)
+else:
+    print(s - ans)
